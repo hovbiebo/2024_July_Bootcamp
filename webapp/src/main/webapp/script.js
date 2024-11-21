bdd02432b05504570a855158927d4cd53be495e1
Henry Ovbiebo <henry.ovbiebo@gmail.com>
2024-11-21 03:58:57 +0000

    Updated the script.jss file

--- webapp/src/main/webapp/script.js
+++ webapp/src/main/webapp/script.js
@@ -1,6 +1,6 @@
 function displayGreeting() {
     var name = document.getElementById('name').value;
     var greetingElement = document.getElementById('greeting');
-    greetingElement.textContent = 'Hello, ' + name + '! Welcome to Mobann DevOps App, Modified by Orchie LLC.';
+    greetingElement.textContent = 'Hello, ' + name + '! Welcome to Mobann DevOps App, Modified by Orchie LLC..';
 }
 
